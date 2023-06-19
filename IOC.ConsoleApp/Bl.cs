06ea47ebe60ff5dcdc6949f8dd1c2c287f1fd9a5
Huseyin Toprak <HuseyinToprak96@outlook.com>
2023-06-19 16:25:51 +0300

    tighly

---
+++ IOC.ConsoleApp/Bl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.ConsoleApp
+{
+    internal class Bl
+    {
+        private Dal _dal { get; set; }
+        public Bl()
+        {
+            _dal = new Dal();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _dal.GetProducts();
+        }
+    }
+}
